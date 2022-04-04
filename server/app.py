b8b8b4c5fa8429f440371a4cead44b673c9c6092
r0neel <ronbhagwakar@gmail.com>
2022-04-04 15:43:23 +0100

    Progress on model

--- server/app.py
+++ server/app.py
@@ -1,19 +1,14 @@
-from crypt import methods
 from flask import Flask, request, jsonify
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 from flask_cors import CORS
-from pymongo import MongoClient
 import hashlib
 import datetime
-from init import db
+from init import init
+from models.User import User
 app = Flask(__name__)
 CORS(app)
+users = init()
 
-client = MongoClient(
-    "mongodb+srv://netlifters:qdfoR8T7tHob6rQj@netlift.mvruz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client['netLift']
-
-users = db['users']
 jwt = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = 'Your_Secret_Key'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
@@ -24,10 +19,10 @@ def home():
     return 'Hello, World!'
 
 
-@app.route('/user/register', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register():
+    print(users)
     new_user = request.get_json()
-    print(new_user, "line 30")
     new_user['password'] = hashlib.sha224(
         new_user["password"].encode("utf-8")).hexdigest()
     users_db = users.find_one({"username": new_user["username"]})
@@ -39,7 +34,7 @@ def register():
         return jsonify({'msg': 'Username already exists'}), 409
 
 
-@app.route('/user/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     login_details = request.get_json()
     user = users.find_one({'username': login_details['username']})
@@ -57,16 +52,18 @@ def login():
 @jwt_required()
 def profile():
     current_user = get_jwt_identity()
-    print(current_user, "from line 61")
-    user_profile = users.find_one({'username': current_user})
-    print(user_profile, "form line 63")
-    if user_profile:
-        # delete data we don't want to return
-        del user_profile['_id'], user_profile['password']
-        return jsonify({'profile': user_profile}), 200
-    else:
-        return jsonify({'msg': 'Profile not found'}), 404
+    print(current_user)
+    user_profile = User.find_by_name(current_user)
+    return jsonify(user_profile), 200
+
+
+# @app.route('/user/all', methods=['GET'])
+# def profile():
+#     user_profile = User.getAll()
+#     return jsonify({'profile': user_profile}), 200
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# user/userid/programs
